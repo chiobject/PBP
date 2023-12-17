3ffa537d6abc74e5462e19505b32e3493a4f01c7
Chiobject <qkrehdwn411@gmail.com>
2023-12-17 17:18:50 +0900

    6+

---
+++ PBP/src/server2/ServerData.java
@@ -0,0 +1,61 @@
+//package server2;
+//
+//import java.io.Serializable;
+//import java.util.UUID;
+//import java.util.Vector;
+//
+//// This class represents the data that will be transmitted between the client and the server
+//public class ServerData implements Serializable {
+//    private String message;
+//    private String userName;
+//    private int state, count, division;
+//    private String uuid;
+//    private Vector<String> v, turn;
+//
+//    // Constants for different states
+//    public static final int LOGIN = 10;    // User login
+//    public static final int START = 20;    // Game start
+//    public static final int LOGOUT = 30;   // User logout
+//    public static final int SENDDATA = 40; // Sending game data
+//    
+//
+//    // Constructor
+//    // Initializes the user name, message, state, and division variables
+//    public ServerData(String Name, String message, int state, int division, UUID uuid) {
+//        this.userName = Name;
+//        this.message = message;
+//        this.state = state;
+//        this.division = division;
+//        this.uuid = uuid.toString();
+//    }
+//
+//    // Getter method for user name
+//    public String getName() {
+//        return this.userName;
+//    }
+//
+//    // Getter method for the message data
+//    public String getMessage() {
+//        return this.message;
+//    }
+//
+//    // Getter method for the state variable
+//    public int getState() {
+//        return this.state;
+//    }
+//
+//    // Getter method for the division variable
+//    public int getDivision() {
+//        return this.division;
+//    }
+//
+//    // Getter method for the log information
+//    public Vector<String> getLoginfo() {
+//        return this.v;
+//    }
+//
+//    // Setter method for log information
+//    public void setLoginfo(Vector<String> v) {
+//        this.v = v;
+//    }
+//}
